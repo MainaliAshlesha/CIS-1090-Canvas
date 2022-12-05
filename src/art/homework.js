ace92e57a09ed68a13d0130ab638df741c439a9d
axm08111 <axm08111@castleton.edu>
2022-12-05 12:14:00 -0500

    smiley face

--- src/art/homework.js
+++ src/art/homework.js
@@ -18,7 +18,7 @@ function shade(x, y, t) {
     //❓❓ Question 2
     //❓❓ Question 3
     //❓❓ Question 4
-    return [1,0,1];
+    return [0.65, 0.45, 0.32];
 }
 
 /**
@@ -28,11 +28,51 @@ function shade(x, y, t) {
  */
 function draw(ctx, t) {
     //See https://developer.mozilla.org/en-US/docs/Web/API/CanvasRenderingContext2D
+
+    function circle(x, y, r) {
+        ctx.beginPath();
+        ctx.arc(x,y,r, 0, 2 * Math.PI);
+    }
+
+    //All lines 2px wide and black
     ctx.lineWidth = 2;
     ctx.strokeStyle = 'black';
-    //❓❓ Question 5
-    //❓❓ Question 6
-    ctx.strokeRect(75, 100, 150, 100);
+
+    //Drawing the head of emoji 
+    circle(100, 100, 40);
+    ctx.fillStyle = 'yellow';
+    ctx.fill();
+    ctx.stroke();
+
+        //Drawing the eye of emoji 
+        circle(80, 100, 10);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+        ctx.stroke();
+
+        //Drawing the eye of emoji 
+        circle(-60, 600, 10);
+        ctx.fillStyle = 'black';
+        ctx.fill();
+        ctx.stroke();
+    
 }
+
+// We will now create snowfall. 
+let addSnow = () => {
+    const random = (min, max) => Math.random * (max - min) + min
+    let screenwidth = px
+    let screenheight = px
+
+    let snow = document.createElement("div");
+    snow.style = "fixed";
+    snow.stylewidth = "10px";
+    snow.styleheigth = "10px";
+    snow.style.backgroundcolor = "white"
+    snow.style.borderRadius = "60%"
+    snow.style.index = "100"
+}
+
+
 
 export default { name: "My Homework", shade, draw }
